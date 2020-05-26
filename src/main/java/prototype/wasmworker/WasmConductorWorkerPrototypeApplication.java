a430ef115490ec575c5542aeeb464a533cc32dfa
Tomas Olvecky <tomas.olvecky@gmail.com>
2020-05-26 21:20:58 +0200

    init

---
+++ src/main/java/prototype/wasmworker/WasmConductorWorkerPrototypeApplication.java
@@ -0,0 +1,13 @@
+package prototype.wasmworker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WasmConductorWorkerPrototypeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WasmConductorWorkerPrototypeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/prototype/wasmworker/WasmConductorWorkerPrototypeApplicationTests.java
@@ -0,0 +1,13 @@
+package prototype.wasmworker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WasmConductorWorkerPrototypeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
